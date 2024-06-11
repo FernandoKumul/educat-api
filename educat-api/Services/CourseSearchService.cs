727f7e54f652029ad391756cf413ec1884b526a5
Luis Flores <luisgomez7829@gmail.com>
2024-06-11 04:17:16 -0500

    feat: Update course search service to include category filtering and pagination parameters

--- educat-api/Services/CourseSearchService.cs
+++ educat-api/Services/CourseSearchService.cs
@@ -17,8 +17,54 @@ namespace educat_api.Services
         {
             int skip = (pageNumber - 1) * pageSize;
 
-            var resultSearch = await _context.Courses
-                .Where(c => (c.Title.Contains(query) || (c.Tags != null && c.Tags.Contains(query))) && c.Active == true)
+            if (category == "all" || category == null)
+            {
+                var resultSearch = await _context.Courses
+                    .Where(c => (c.Title.Contains(query) || (c.Tags != null && c.Tags.Contains(query))) && c.Active == true)
+                    .Join(
+                        _context.Instructors,
+                        c => c.FKInstructor,
+                        i => i.PkInstructor,
+                        (c, i) => new { c, i }
+                    )
+                    .Join(
+                        _context.Users,
+                        ci => ci.i.FkUser,
+                        u => u.PkUser,
+                        (ci, u) => new { ci.c, ci.i, InstructorName = u.Name }
+                    )
+                    .Join(
+                        _context.Categories,
+                        ciu => ciu.c.FkCategory,
+                        cat => cat.PkCategory,
+                        (ciu, cat) => new CourseSearchDTO
+                        {
+                            PkCourse = ciu.c.PkCourse,
+                            Title = ciu.c.Title,
+                            Difficulty = ciu.c.Difficulty,
+                            Price = ciu.c.Price,
+                            Active = ciu.c.Active,
+                            Tags = ciu.c.Tags,
+                            FKInstructor = ciu.c.FKInstructor,
+                            InstructorName = ciu.InstructorName,
+                            FkCategory = ciu.c.FkCategory,
+                            CategoryName = cat.Name
+                        }
+                    )
+                    .Skip(skip)
+                    .Take(pageSize)
+                    .ToListAsync();
+
+                var count = await _context.Courses
+                    .Where(c => (c.Title.Contains(query) || (c.Tags != null && c.Tags.Contains(query))) && c.Active == true)
+                    .CountAsync();
+                    
+                return (resultSearch, count);
+
+            }
+            else
+            {
+                var filteredCategories = await _context.Courses
                 .Join(
                     _context.Instructors,
                     c => c.FKInstructor,
@@ -49,20 +95,21 @@ namespace educat_api.Services
                         CategoryName = cat.Name
                     }
                 )
+                .Where(c => ((c.Title != null && c.Title.Contains(query)) || (c.Tags != null && c.Tags.Contains(query))) && c.Active == true && c.CategoryName == category)
                 .Skip(skip)
                 .Take(pageSize)
                 .ToListAsync();
-            var count = await _context.Courses
-                .Where(c => (c.Title.Contains(query) || (c.Tags != null && c.Tags.Contains(query))) && c.Active == true)
-                .CountAsync();
-        
-            if (category == "all" || category == null)
-            {
-                return (resultSearch, count);
 
-            } else
-            {
-                var filteredCategories = resultSearch.Where(c => c.CategoryName == category);
+                var count = await _context.Courses
+                    .Join(
+                        _context.Categories,
+                        c => c.FkCategory,
+                        cat => cat.PkCategory,
+                        (c, cat) => new { c, cat }
+                    )
+                    .Where(c => ((c.c.Title != null && c.c.Title.Contains(query)) || (c.c.Tags != null && c.c.Tags.Contains(query))) && c.c.Active == true && c.cat.Name == category)
+                    .CountAsync();
+
                 return (filteredCategories, count);
             }
         }
