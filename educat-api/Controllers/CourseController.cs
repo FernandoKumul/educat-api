bf98207eaf938bdc09999f08d18144e6a3f7426c
FernandoKumul <ferdesarrollador11@outlook.com>
2024-06-18 00:44:40 -0500

    Edit Course

--- educat-api/Controllers/CourseController.cs
+++ educat-api/Controllers/CourseController.cs
@@ -60,5 +60,70 @@ namespace educat_api.Controllers
             }
         }
 
+        [Authorize]
+        [HttpPut("save-draft/{id}")]
+        public async Task<ActionResult<Response<string>>> Update(int id, [FromBody] CourseSaveInDTO updateCourse)
+        {
+
+            //for (int i = 0; i < updateTest.Questions.Count; i++)
+            //{
+            //    int nCorrect = 0;
+            //    foreach (var answer in updateTest.Questions[i].Answers)
+            //    {
+            //        if (answer.Correct) nCorrect++;
+            //    }
+
+            //    if (nCorrect == 0)
+            //    {
+            //        return BadRequest(new Response<string>(false, $"La pregunta[{i}] no tiene ninguna respuesta correcta"));
+            //    }
+            //}
+
+            //string[] visibilityTypes = { "unlisted", "private", "public" };
+            //if (!Array.Exists(visibilityTypes, color => color == updateTest.Visibility))
+            //{
+            //    return BadRequest(new Response<int>(false, "No está ingresando algún tipo de visibilidad valido"));
+            //}
+
+            //string[] colors = { "green", "blue", "purple", "orange", "yellow", "red" };
+
+            //if (!Array.Exists(colors, color => color == updateTest.Color))
+            //{
+            //    return BadRequest(new Response<int>(false, "No está ingresando algún color valido"));
+            //}
+
+            var payloadId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+
+            try
+            {
+                var userId = User.FindFirst("ID")?.Value;
+                var isInstructor = User.FindFirst("IsInstructor")?.Value;
+
+                if (!Int32.TryParse(userId, out int userIdInt))
+                {
+                    return new ObjectResult(new Response<string>(false, "Token no válido")) { StatusCode = 403 };
+
+                }
+
+                if (!Boolean.TryParse(isInstructor, out bool isInstructorBool))
+                {
+                    return new ObjectResult(new Response<string>(false, "Token no válido")) { StatusCode = 403 };
+                }
+
+                if (!isInstructorBool)
+                {
+                    return new ObjectResult(new Response<string>(false, "No eres un instructor")) { StatusCode = 403 };
+                }
+
+                await _service.SaveCourse(updateCourse, id, userIdInt);
+
+                return Ok(new Response<string>(true, "Curso guardado de manera exitosa"));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new Response<string>(false, ex.Message, ex.InnerException?.Message ?? "")); //Cambiar por un 500 luego :D
+            }
+        }
+
     }
 }
