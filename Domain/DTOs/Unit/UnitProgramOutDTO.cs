e96dc6cacb4c443ecdf97d23ccea777e03f87008
FernandoKumul <ferdesarrollador11@outlook.com>
2024-06-22 02:45:24 -0500

    Get info public course

---
+++ Domain/DTOs/Unit/UnitProgramOutDTO.cs
@@ -0,0 +1,19 @@
+﻿using Domain.DTOs.Lesson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.DTOs.Unit
+{
+    public class UnitProgramOutDTO
+    {
+        public int PkUnit { get; set; }
+        public int FkCourse { get; set; }
+        public int Order { get; set; }
+        public string Title { get; set; } = null!;
+
+        public ICollection<LessonProgramOutDTO> Lessons { get; set; } = new List<LessonProgramOutDTO>();
+    }
+}
--- educat-api/Program.cs
+++ educat-api/Program.cs
@@ -8,11 +8,14 @@ using System.Text;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//builder.Configuration.AddEnvironmentVariables();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+Console.WriteLine(builder.Configuration.GetConnectionString("DefaultConnection"));
 
 builder.Services.AddDbContext<AppDBContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
