e7cc48be167b0f83964e04f5661b942874397d50
FernandoKumul <ferdesarrollador11@outlook.com>
2024-07-15 19:41:00 -0500

    Show rating and lastname in courseSearch

--- Domain/DTOs/Course/CourseSearchDTO.cs
+++ Domain/DTOs/Course/CourseSearchDTO.cs
@@ -8,10 +8,12 @@ namespace Domain.DTOs.Course
         public string? Difficulty { get; set; }
         public string? Cover { get; set; }
         public decimal? Price { get; set; }
+        public decimal? Rating { get; set; } = 0;
         public bool? Active { get; set; }
         public string? Tags { get; set; }
         public int? FKInstructor { get; set; }
         public string? InstructorName { get; set; }
+        public string? InstructorLastName { get; set; }
         public int? FkCategory { get; set; }
         public string? CategoryName { get; set; }
     }
